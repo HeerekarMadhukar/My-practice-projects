1206fc8fafa8e1a9eddaeaaef67341aee2cfa3b1
hmadhuka <98558448+hmadhuka@users.noreply.github.com>
2023-04-20 13:11:16 -0400

    Add files via upload
    I visualize the Indian flag using library turtle.

---
+++ Indian flag by madhukar.py
@@ -0,0 +1,97 @@
+import turtle
+from turtle import *
+
+# output
+screen = turtle.Screen()
+
+# instances
+t = turtle.Turtle()
+speed(100)
+
+# initially penup()
+t.penup()
+t.goto(-400, 250)
+t.pendown()
+
+# Orange and white rectangles
+t.color("orange")
+t.begin_fill()
+t.forward(800)
+t.right(90)
+t.forward(167)
+t.speed(30)
+t.right(90)
+t.forward(800)
+t.end_fill()
+t.left(90)
+t.forward(167)
+
+
+# Green Rectangle
+t.color("green")
+t.begin_fill()
+t.forward(167)
+t.left(90)
+t.speed(30)
+t.forward(800)
+t.left(90)
+t.forward(167)
+t.end_fill()
+
+# Blue Circle
+t.penup()
+t.goto(70, 0)
+t.pendown()
+t.color("navy")
+t.begin_fill()
+t.circle(70)
+t.end_fill()
+
+# White Circle
+t.penup()
+t.goto(60, 0)
+t.pendown()
+t.color("white")
+t.begin_fill()
+t.circle(60)
+t.end_fill()
+
+# small Blue Circles
+t.penup()
+t.speed(30)
+t.goto(-57, -8)
+t.pendown()
+t.color("navy")
+for i in range(24):
+    t.begin_fill()
+    t.circle(3)
+    t.end_fill()
+    t.penup()
+    t.forward(15)
+    t.right(15)
+    t.pendown()
+
+# mini Blue Circle
+t.penup()
+t.goto(20, 0)
+t.speed(20)
+t.pendown()
+t.begin_fill()
+t.circle(20)
+t.end_fill()
+# Spokes
+t.penup()
+t.goto(0, 0)
+t.pendown()
+t.pensize(2)
+t.speed(20)
+for i in range(24):
+    t.forward(60)
+    t.backward(60)
+    t.left(15)
+
+# to hold the
+# output window
+turtle.done()
+
+
